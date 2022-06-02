818efdda14db24f2a1fbda1b81f43ed8cee493f7
Bekir - CDI <b.burakcebecii@gmail.com>
2022-06-02 11:06:14 +0200

    Page General book created..

---
+++ src/pages/PageGeneralBooks.jsx
@@ -0,0 +1,8 @@
+export const PageGeneralBooks = () => {
+  return (
+    <div className="page_generalBooks">
+      <h2>General Books</h2>
+      <p>This is the general book page.</p>
+    </div>
+  );
+};
