52b1caa1ba760aeae28341e2bc0291f5c4666cc1
Bekir - CDI <b.burakcebecii@gmail.com>
2022-06-02 11:52:18 +0200

    withLoadedBooks changed..

--- src/hocs/withLoadedBooks.jsx
+++ src/hocs/withLoadedBooks.jsx
@@ -4,31 +4,29 @@ import axios from "axios";
 const techBookUrl = "https://edwardtanguay.netlify.app/share/techBooks.json";
 const generalBookUrl = "https://edwardtanguay.netlify.app/share/books.json";
 
-export const withLoadedBooks = (Component, subtitle) => {
-  return (props) => {
-    const [techBooks, setTechBooks] = useState([]);
-    const [generalBooks, setGeneralBooks] = useState([]);
-    const [subtitle, setSubtitle] = useState([]);
-    useEffect(() => {
-      (async () => {
-        setTechBooks((await axios.get(techBookUrl)).data);
-      })();
-      setSubtitle(subtitle);
-    }, []);
-    useEffect(() => {
-      (async () => {
-        setGeneralBooks((await axios.get(generalBookUrl)).data);
-      })();
-      setSubtitle(subtitle);
-    }, []);
+export const withLoadedBooks = (Component, subtitle) => (props) => {
+  const [techBooks, setTechBooks] = useState([]);
+  const [generalBooks, setGeneralBooks] = useState([]);
 
-    return (
-      <Component
-        {...props}
-        subtitle={subtitle}
-        techBooks={techBooks}
-        generalBooks={generalBooks}
-      />
-    );
-  };
+  useEffect(() => {
+    (async () => {
+      setTechBooks((await axios.get(techBookUrl)).data);
+    })();
+    setSubtitle(subtitle);
+  }, []);
+  useEffect(() => {
+    (async () => {
+      setGeneralBooks((await axios.get(generalBookUrl)).data);
+    })();
+    setSubtitle(subtitle);
+  }, []);
+
+  return (
+    <Component
+      {...props}
+      subtitle={subtitle}
+      techBooks={techBooks}
+      generalBooks={generalBooks}
+    />
+  );
 };
