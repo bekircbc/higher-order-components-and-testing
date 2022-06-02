399bad7755479ed0291f342ad067062db883abbf
Bekir - CDI <b.burakcebecii@gmail.com>
2022-06-02 11:58:33 +0200

    quote and books added..

--- src/pages/PageTechBooks.jsx
+++ src/pages/PageTechBooks.jsx
@@ -1,9 +1,18 @@
-export const PageTechBooks = ({ techBooks, subtitle }) => {
+export const PageTechBooks = ({ techBooks, subtitle, quote }) => {
   return (
     <div className="page_techBooks">
       <h2>Tech Books</h2>
       <h3>{subtitle}</h3>
-      <p>There are {techBooks.length} tech books.</p>
+
+      <p>
+        <i>"{quote}"</i>
+      </p>
+      <p>Here are the {generalBooks.length} general books:</p>
+      <ul>
+        {generalBooks.map((book, index) => {
+          return <li key={index}>{book.title}</li>;
+        })}
+      </ul>
     </div>
   );
 };
