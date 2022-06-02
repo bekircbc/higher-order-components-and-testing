7e6200ccbddb3754a8821d0ed01ede249e470601
Bekir - CDI <b.burakcebecii@gmail.com>
2022-06-02 11:19:06 +0200

    with Loaded Books high order comonent added to page..

---
+++ src/hocs/withLoadedBooks.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const techBookUrl = "https://edwardtanguay.netlify.app/share/techBooks.json";
+const generalBookUrl = "https://edwardtanguay.netlify.app/share/books.json";
+
+export const withLoadedBooks = () => {
+  return (props) => {
+    const [techBooks, setTechBooks] = useState([]);
+    const [generalBooks, setGeneralBooks] = useState([]);
+
+    useEffect(() => {
+      (async () => {
+        setTechBooks((await axios.get(techBookUrl)).data);
+      })();
+    }, []);
+    useEffect(() => {
+      (async () => {
+        setGeneralBooks((await axios.get(generalBookUrl)).data);
+      })();
+    }, []);
+
+    return (
+      <Component {...props} techBooks={techBooks} generalBooks={generalBooks} />
+    );
+  };
+};
