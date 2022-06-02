580957b8f3a72bfcdafe9db40dfc3426fc07b9b9
Bekir - CDI <b.burakcebecii@gmail.com>
2022-06-02 11:16:28 +0200

    Links and Route added..

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
-import "./App.css";
+import "./App.scss";
+import { PageWelcome } from "./pages/PageWelcome";
+import { PageTechBooks } from "./pages/PageTechBooks";
+import { PageGeneralBooks } from "./pages/PageGeneralBooks";
+import { PageAbout } from "./pages/PageAbout";
+import { NavLink, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
-  return <div className="App">hallo</div>;
+  return (
+    <div className="App">
+      <h1>Personal Site</h1>
+      <hr />
+      <NavLink to="/welcome">Welcome</NavLink> |{" "}
+      <NavLink to="/tech-books">Tech Books</NavLink> |{" "}
+      <NavLink to="/general-books">General Books</NavLink> |{" "}
+      <NavLink to="/about">About</NavLink>
+      <hr />
+      <Routes>
+        <Route path="/welcome" element={<PageWelcome />} />
+        <Route path="/tech-books" element={<PageTechBooks />} />
+        <Route path="/general-books" element={<PageGeneralBooks />} />
+        <Route path="/about" element={<PageAbout />} />
+        <Route path="/" element={<Navigate to="/welcome" replace />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
